58732d353a6dce018709e88b114e3fa46fd25086
Prawee Wongsa <prawee@hotmail.com>
2014-07-09 15:29:08 +0700

    Added urlManager components.

--- common/config/main.php
+++ common/config/main.php
@@ -1,8 +1,14 @@
 <?php
+
 return [
     'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
     'extensions' => require(__DIR__ . '/../../vendor/yiisoft/extensions.php'),
     'components' => [
+        'urlMananger' => [
+            'class' => 'yii\web\UrlManager',
+            'enablePrettyUrl' => true,
+            'showScriptName' => false,
+        ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
